c56c6e24bbbd9f1b2b0a5623a172c36836fc98d3
Sneha Manandhar <snehamanandhar54@gmail.com>
2026-09-20 21:10:19 +0545

    Update API proxy URLs in vite.config.js to point to the correct backend server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      "/api": "https://link-in-two.vercel.app",
-      "/uploads": "https://link-in-two.vercel.app",
+      "/api": "https://linkin-suda.onrender.com",
+      "/uploads": "https://linkin-suda.onrender.com",
     },
   },
 })
